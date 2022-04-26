9052d0d241ee5136cc433cf1684321c3edbad6fe
tundeiness <tundeiness@yahoo.co.uk>
2022-04-26 10:03:15 +0100

    install tailwindcss
    remove excess files
    
    install eslint
    
    lint files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header text-3xl font-bold underline">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
