ddf4f1d83e505adb90430dfbf106b05444bae32b
tundeiness <tundeiness@yahoo.co.uk>
2022-04-26 11:07:14 +0100

    populate screens in tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex justify-center items-center border border-amber-400 h-screen w-screen">
       <header className="App-header text-3xl font-bold underline">
         <p>
           Hello World!
