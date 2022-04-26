45f10d1d8f70798a37aecf36f00f777cfa9d3ca7
tundeiness <tundeiness@yahoo.co.uk>
2022-04-26 10:14:29 +0100

    create components folder

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header text-3xl font-bold underline">
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
