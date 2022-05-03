485966c995ca25378aee5f723da72070f18e833e
tundeiness <tundeiness@yahoo.co.uk>
2022-05-03 14:30:22 +0100

    edit index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter forceRefresh>
     <React.StrictMode>
       <App />
     </React.StrictMode>
