4ec6b042ccef10ed806b7ff8a2000daf9c56e8fa
tundeiness <tundeiness@yahoo.co.uk>
2022-05-07 19:29:52 +0100

    complete lg screen design

--- src/Views/password-reset/password-reset.jsx
+++ src/Views/password-reset/password-reset.jsx
@@ -46,7 +46,7 @@ const PasswordReset = () => {
               <img src={AeonLogo} alt="aeon-logo" className="xl:w-56 xl:h-14 lg:w-56 lg:h-14 sm:w-56 sm:h-14 xs:w-56 xs:h-14" />
             </div>
 
-            <div className="flex flex-row justify-center border border-blue-500 w-full xl:mt-16 lg:mt-8 md:mt-10 xs:mt-24 xs:mb-4">
+            <div className="flex flex-row justify-center border border-blue-500 w-full xl:mt-16 lg:mt-10 md:mt-10 sm:mt-12 xs:mt-24 xs:mb-4">
               <div className="key-wrapper flex flex-col justify-center items-center w-14 h-14 rounded-full bg-indigo-100 border border-red-500">
                 <div className="flex flex-col justify-center items-center bg-green-100 w-12 h-12 rounded-full">
                   <div className="flex flex-col justify-center items-center bg-green-200 w-9 h-9 rounded-full">
@@ -57,8 +57,8 @@ const PasswordReset = () => {
             </div>
 
             <div className="w-full h-100 xl:px-16 lg:px-16 lg:mb-0 xs:mb-14">
-              <h1 className="cta-heading xl:mt-2 lg:mt-2 text-center font-semibold text-gray-900 leading-8 xl:text-3xl lg:text-3xl xl:font-semibold xs:font-semibold md:font-semibold md:text-2xl sm:text-2xl xs:text-2xl">Password reset</h1>
-              <h2 className="cta-sub-heading sm:text-sm text-gray500 mt-5 text-center xl:font-normal xl:text-lg lg:font-normal lg:text-sm xs:font-normal xs:leading-6 xs:text-base xl:px-6 lg:px-4 md:px-1 sm:px-3 xs:px-3 md:font-normal outline outline-red-600">
+              <h1 className="cta-heading xl:mt-2 lg:mt-2 text-center text-gray-900 leading-8 xl:text-3xl lg:text-3xl xl:font-semibold lg:font-semibold xs:font-semibold md:font-semibold md:text-2xl sm:text-2xl xs:text-2xl">Password reset</h1>
+              <h2 className="cta-sub-heading text-gray500 mt-5 text-center xl:font-normal xl:text-lg lg:font-normal lg:text-base xs:font-normal xs:leading-6 xs:text-base xl:px-6 lg:px-4 md:px-1 sm:px-3 xs:px-3 md:font-normal md:text-base sm:font-normal sm:text-base outline outline-red-600">
                 Your password has been successfully reset. Click below to log in magically.
               </h2>
               <form className="lg:h-0 sm:h-screen mt-6 xl:px-6 lg:px-6 sm:px-4" onSubmit={formic.handleSubmit}>
