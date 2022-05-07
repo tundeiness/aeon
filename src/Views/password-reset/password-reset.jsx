3986e4183be69ec0d70dc0a3754a56f8f33ef3cd
tundeiness <tundeiness@yahoo.co.uk>
2022-05-07 19:50:24 +0100

    edit password-reset file

--- src/Views/password-reset/password-reset.jsx
+++ src/Views/password-reset/password-reset.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-import React, { Suspense } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   useFormik,
 } from 'formik';
@@ -10,44 +10,19 @@ import { FiCheckCircle } from 'react-icons/fi';
 import AeonLogo from '../../static/assets/img/logo-blue.png';
 
 const PasswordReset = () => {
-  const validate = (value) => {
-    const errors = {};
-    // const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-    if (!value.email) {
-      errors.email = 'Cannot be blank';
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)
-    ) {
-      errors.email = 'Invalid email format';
-    }
-
-    return errors;
-  };
-
-  const formic = useFormik({
-    initialValues: {
-      email: '',
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(`You have loggedin succesfully! Email: ${values.email}`);
-    },
-  });
-
-  console.log(formic.values);
+  const test = 0;
   return (
     <main className="check-container flex flex-col justify-between">
       <article className="check-article-matter flex flex-col items-center justify-center">
-        <section className="check-section bg-white flex lg:h-screen md:mx-auto md-mx-0 md:max-w-md lg:max-w-full w-full md:w-1/2 px-6  xl:px-20 xl:mx-10 lg:px-4 lg:mx-2 border border-blue-700">
-          <div className="section-content-wrap w-full flex flex-col xl:mt-16 lg:mt-20 border border-red-900">
+        <section className="check-section bg-white flex lg:h-screen md:mx-auto md-mx-0 md:max-w-md lg:max-w-full w-full md:w-1/2 px-6  xl:px-20 xl:mx-10 lg:px-4 lg:mx-2">
+          <div className="section-content-wrap w-full flex flex-col xl:mt-16 lg:mt-20">
 
-            <div className="logo-container flex justify-center xl:mb-0 lg:mb-8 sm:mb-4 lg:mt-0 md:mt-20 sm:mt-20 xs:mt-20 border border-red-900">
+            <div className="logo-container flex justify-center xl:mb-0 lg:mb-8 sm:mb-4 lg:mt-0 md:mt-20 sm:mt-20 xs:mt-20">
               <img src={AeonLogo} alt="aeon-logo" className="xl:w-56 xl:h-14 lg:w-56 lg:h-14 sm:w-56 sm:h-14 xs:w-56 xs:h-14" />
             </div>
 
-            <div className="flex flex-row justify-center border border-blue-500 w-full xl:mt-16 lg:mt-10 md:mt-10 sm:mt-12 xs:mt-24 xs:mb-4">
-              <div className="key-wrapper flex flex-col justify-center items-center w-14 h-14 rounded-full bg-indigo-100 border border-red-500">
+            <div className="flex flex-row justify-center w-full xl:mt-16 lg:mt-10 md:mt-10 sm:mt-12 xs:mt-24 xs:mb-4">
+              <div className="key-wrapper flex flex-col justify-center items-center w-14 h-14 rounded-full bg-indigo-100">
                 <div className="flex flex-col justify-center items-center bg-green-100 w-12 h-12 rounded-full">
                   <div className="flex flex-col justify-center items-center bg-green-200 w-9 h-9 rounded-full">
                     <FiCheckCircle className="font-bold text-2xl xs:text-3xl text-green-400 " />
@@ -58,106 +33,14 @@ const PasswordReset = () => {
 
             <div className="w-full h-100 xl:px-16 lg:px-16 lg:mb-0 xs:mb-14">
               <h1 className="cta-heading xl:mt-2 lg:mt-2 text-center text-gray-900 leading-8 xl:text-3xl lg:text-3xl xl:font-semibold lg:font-semibold sm:font-semibold xs:font-semibold md:font-semibold md:text-2xl sm:text-2xl xs:text-2xl">Password reset</h1>
-              <h2 className="cta-sub-heading text-gray500 mt-5 text-center xl:font-normal xl:text-lg lg:font-normal lg:text-base xs:font-normal xs:leading-6 xs:text-base xl:px-6 lg:px-4 md:px-1 sm:px-3 xs:px-3 md:font-normal md:text-base sm:font-normal sm:text-base outline outline-red-600">
+              <h2 className="cta-sub-heading text-gray500 mt-5 text-center xl:font-normal xl:text-lg lg:font-normal lg:text-base xs:font-normal xs:leading-6 xs:text-base xl:px-6 lg:px-4 md:px-1 sm:px-3 xs:px-3 md:font-normal md:text-base sm:font-normal sm:text-base">
                 Your password has been successfully reset. Click below to log in magically.
               </h2>
-              <form className="lg:h-0 sm:h-screen mt-6 xl:px-6 lg:px-6 sm:px-4" onSubmit={formic.handleSubmit}>
-                {/* <div>
-                  <label className="block text-gray-700">Email</label>
-                  <input
-                    className={`w-full bg-white mt-2 border border-gray-400 focus:border-blue-500 focus:bg-white  focus:outline-none focus:shadow-outline rounded-lg px-4 py-3 ${
-                      formic.email && formic.errors.email
-                        ? 'border-red-400'
-                        : 'border-gray-100'
-                    } `}
-                    onChange={formic.handleChange}
-                    onBlur={formic.handleBlur}
-                    value={formic.values.email}
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    autoComplete="off"
-                    required
-                  />
-                  {formic.touched.email && formic.errors.email && (
-                  <span className="text-red-300 text-xs">
-                    {formic.errors.email}
-                  </span>
-                  )}
-                </div> */}
-
-                {/* <div className="mt-4">
-                  <label className="block text-gray-700">Password</label>
-                  <input
-                    className={`w-full bg-white mt-2 border border-gray-300 focus:border-blue-500 focus:bg-white  focus:outline-none focus:shadow-outline rounded-lg px-4 py-3 ${
-                      formic.password && formic.errors.password
-                        ? 'border-red-300'
-                        : 'border-gray-300'
-                    }`}
-                    onChange={formic.handleChange}
-                    onBlur={formic.handleBlur}
-                    value={formic.values.password}
-                    id="password"
-                    name="password"
-                    type="password"
-                    minLength={8}
-                    placeholder="Enter password"
-                    required
-                  />
-                  {formic.touched.password && formic.errors.password && (
-                  <span className="text-red-400">{formic.errors.password}</span>
-                  )}
-                  <p className="xl:mt-1 lg:mt-2 md:mt-3 text-base font-normal text-gray-500">Must be at least 8 characters</p>
-                </div> */}
-
-                {/* <div className="mt-4">
-                  <label className="block text-gray-700">Confirm password</label>
-                  <input
-                    className={`w-full  mt-2 border border-gray-300 focus:border-blue-500 focus:bg-white  focus:outline-none focus:shadow-outline rounded-lg px-4 py-3 ${
-                      formic.password && formic.errors.password
-                        ? 'border-red-300'
-                        : 'border-gray-300'
-                    }`}
-                    onChange={formic.handleChange}
-                    onBlur={formic.handleBlur}
-                    value={formic.values.password}
-                    id="password"
-                    name="password"
-                    type="password"
-                    minLength={8}
-                    placeholder="Enter password"
-                    required
-                  />
-                  {formic.touched.password && formic.errors.password && (
-                  <span className="text-red-400">{formic.errors.password}</span>
-                  )}
-                </div> */}
-
-                {/* <div className="flex flex-row lg:justify-center justify-between w-full mt-12">
-                  <div className="flex md:hidden flex-row">
-                    <input id="remember" className="remember-input mt-2 mr-2" type="checkbox" />
-                    <label
-                      className="label block text-gray-700 text-xs font-medium mt-1"
-                      htmlFor="remember"
-                      id="remember-label"
-                    >
-                      Remember for 30 days
-                    </label>
-                  </div>
-                  <Link className="block font-medium text-forgotBlue hover:text-black focus:text-black focus:outline-none" to="/forgot-password">Forgot Password</Link>
-                </div> */}
+              <form className="lg:h-0 sm:h-screen mt-6 xl:px-6 lg:px-6 sm:px-4">
 
                 <button className="sign-in-button w-full block bg-buttonBlueDeep text-white hover:bg-blue-700 px-4 py-3 mt-8 rounded-lg font-medium focus:bg-blue-700 focus:outline-none" type="submit">Continue</button>
-                {/* <div className="flex flex-row justify-center xl:mt-8 lg:mt-8 md:mt-6 sm:mt-6 xs:mt-6 border border-red-700">
-                  <p className="text-base text-gray-500 pr-2">
-                    Didn&apos;t recieve the email?
-                    {' '}
-                  </p>
-                  <Link className="inline-block text-link" to="/forgot-password">click to resend</Link>
 
-                </div> */}
-                <div className="back-to-login flex flex-row mt-6 justify-center border border-red-600">
+                <div className="back-to-login flex flex-row mt-6 justify-center">
                   <Link className="text-gray-500" to="/">
                     <span className="back-arrow inline-block pr-2 text-md">&larr;</span>
                     <span className="inline-block">Back to Log in</span>
@@ -167,19 +50,7 @@ const PasswordReset = () => {
             </div>
           </div>
         </section>
-
-        {/* <section className="relative banner-section hidden md:w-1/2 lg:block">
-          <div className="relative image-wrapper w-full">
-            <img src={LandingImage} alt="landing" className="w-full h-full object-cover" />
-          </div>
-          <img className="absolute bottom-0 w-full object-cover" src={LogoImage} alt="transparent-logo" />
-        </section> */}
       </article>
-      {/* <footer className="hidden lg:block footer-matter py-0 font-medium text-gray-500">
-        {' '}
-        <span className="inline-block pl-5">&copy;</span>
-        <span className="uppercase inline-block pr-3">Credequity 2022</span>
-      </footer> */}
     </main>
   );
 };
