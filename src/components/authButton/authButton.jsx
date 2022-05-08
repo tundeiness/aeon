1214ccd5826cd38d2c801c8b412070366ce0aa16
tundeiness <tundeiness@yahoo.co.uk>
2022-05-08 21:10:42 +0100

    refactor button component

--- src/components/authButton/authButton.jsx
+++ src/components/authButton/authButton.jsx
@@ -1,23 +1,24 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/require-default-props */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function AuthButton({
-  type, buttonText, onClick, loading, className = '',
-}) {
+const AuthButton = ({
+  buttonType, buttonText, loading,
+}) => {
   const test = 0;
   return (
-    <div>authButton</div>
+    <button className="sign-in-button w-full block bg-buttonBlueDeep text-white hover:bg-blue-700 px-4 py-3 mt-8 rounded-lg font-medium focus:bg-blue-700 focus:outline-none" type={buttonType}>
+      {buttonText}
+    </button>
   );
-}
+};
 
 AuthButton.propTypes = {
-  type: PropTypes.string.isRequired,
+  buttonType: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
   loading: PropTypes.bool.isRequired,
-  className: PropTypes.string.isRequired,
 };
 
 export default AuthButton;
