6d222660a72c4e9533c3957612460f3bd1c3635f
tundeiness <tundeiness@yahoo.co.uk>
2022-05-08 20:50:27 +0100

    create authButton component

---
+++ src/components/authButton/authButton.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AuthButton({
+  type, buttonText, onClick, loading, className = '',
+}) {
+  const test = 0;
+  return (
+    <div>authButton</div>
+  );
+}
+
+export default AuthButton;
