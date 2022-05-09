13de95dda15dc8b3c586f04bc5c145e18bed83b9
tundeiness <tundeiness@yahoo.co.uk>
2022-05-09 10:06:05 +0100

    create pageLoader

---
+++ src/components/pageLoader/pageLoader.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const PageLoader = () => {
+  const test = 0;
+  return (
+    <div>pageLoader</div>
+  );
+};
+
+export default PageLoader;
